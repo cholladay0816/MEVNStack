5ed6cb4b4e711a28bcbb683c84e46287bad00894
Chris H <cholladay0816@gmail.com>
2021-02-07 22:26:25 -0600

    Initial commit

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+
+app.listen(process.env.PORT || 8081)
